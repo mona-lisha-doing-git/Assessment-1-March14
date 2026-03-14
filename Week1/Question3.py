de420f7e27fd6866d9305ff9a96d5588c47cdf60
Amazon-Clone <monalisha.k559@gmail.com>
2026-03-14 14:53:02 +0530

    Question3 - Naukri.com

--- Week1/Question3.py
+++ Week1/Question3.py
@@ -1,7 +1,7 @@
 # 14-03-2026 (Saturday)
 
 '''
-Automate Search on YouTube Using Name Locator
+Automate Text Field Entry Using ID Locator
 '''
 
 from selenium.webdriver import Chrome, ChromeOptions
@@ -11,7 +11,17 @@ from time import sleep
 o=ChromeOptions()
 o.add_experimental_option('detach', True)
 driver = Chrome(options=o)
-driver.get("https://www.youtube.com")
+driver.get("https://www.naukri.com")
 driver.maximize_window()
 sleep(1)
 
+driver.find_element(By.ID, "register_Layer").click()
+sleep(2)
+
+driver.find_element(By.ID, "name").send_keys("Monalisha Kalita")
+sleep(2)
+driver.find_element(By.ID, "email").send_keys("monalisha@gmail.com")
+sleep(2)
+driver.find_element(By.ID, "password").send_keys("This is a password input!")
+sleep(5)
+driver.quit()
