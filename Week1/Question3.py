7fd1aaf96a5cd40d0305c7bd839d464c8e7948f6
Amazon-Clone <monalisha.k559@gmail.com>
2026-03-14 14:43:42 +0530

    Question2 - Youtube

--- Week1/Question3.py
+++ Week1/Question3.py
@@ -15,7 +15,3 @@ driver.get("https://www.youtube.com")
 driver.maximize_window()
 sleep(1)
 
-driver.find_element(By.CSS_SELECTOR, "input[name='search_query']").send_keys("melody hits")
-driver.find_element(By.CSS_SELECTOR, "button[aria-label='Search']").click()
-sleep(5)
-driver.quit()
